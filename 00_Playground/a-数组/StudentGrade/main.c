a6b8b73512bfda1e0c35ca80bd7b7fb63ec7e81d
xp <13479908448@163.com>
2026-01-22 17:11:40 +0800

    添加了过滤vs2022的规则，改进了学生数组的练习bug

--- 00_Playground/a-数组/StudentGrade/main.c
+++ 00_Playground/a-数组/StudentGrade/main.c
@@ -4,14 +4,20 @@
 int main()
 {
 	int max = 0;
+	float averge = 0;
+	float sum = 0;
 	int sorces[5];
 	for (int i = 0; i < 5;i++) {
 		printf("请输入第%d个学生的成绩:\n",i+1 );
 		scanf_s("%d", &sorces[i]);
-		if (i >= 1 && max < sorces[i-1]) {
-			max = sorces[i-1];
+		if (max < sorces[i]) {
+			max = sorces[i];
 		}
+		sum = sorces[i] + sum;
+		averge =sum / (i + 1);
 	}
-	printf("最大数是:%d", max);
+	printf("最好的成绩是:%d\n", max);
+	printf("总分数:%f\n", sum);
+	printf("平均成绩是:%f\n", averge);
 	return 0;
 }
