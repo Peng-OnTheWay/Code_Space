db201ab422d40af064110b6d54e4ab9fc78e0cc8
xp <13479908448@163.com>
2026-01-26 11:16:41 +0800

    新增了链表

---
+++ 00_Playground/b-链表/StudentExcel/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Student {
+	char id[10];
+	char name[10];
+	int age;
+	char sex[1];
+}Student;
+
+//∂®“Â¡¥±Ì
+typedef struct Node {
+	Student data;
+	struct Node* next;
+}Node;
+
+int main() {
+
+
+
+}
